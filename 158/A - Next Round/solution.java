04d8d651b5f6fbea4b0f6d4c7b57b4bea88b103a
HERAMB KRISHNA ARORA <heramb.24bce10154@vitbhopal.ac.in>
2026-07-28 19:20:43 +0530

    Add Next Round [A] from Codeforces

---
+++ 158/A - Next Round/solution.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+ 
+public class NextRound {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+ 
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+ 
+        int threshold = a[k - 1]; 
+        int count = 0;
+ 
+        for (int i = 0; i < n; i++) {
+            if (a[i] >= threshold && a[i] > 0) {
+                count++;
+            }
+        }
+ 
+        System.out.println(count);
+    }
+}
