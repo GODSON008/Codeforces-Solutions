440087505c1921e0d3fc8dfce07b94f13200397d
HERAMB KRISHNA ARORA <heramb.24bce10154@vitbhopal.ac.in>
2026-07-29 23:15:25 +0530

    Add Team [A] from Codeforces

---
+++ 231/A - Team/solution.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        if (scanner.hasNextInt()) {
+            int n = scanner.nextInt();
+            int implementedProblems = 0;
+            
+            
+            for (int i = 0; i < n; i++) {
+               
+                int petya = scanner.nextInt();
+                int vasya = scanner.nextInt();
+                int tonya = scanner.nextInt();
+                
+              
+                if (petya + vasya + tonya >= 2) {
+                    implementedProblems++;
+                }
+            }
+            
+            
+            System.out.println(implementedProblems);
+        }
+        
+        scanner.close();
+    }
+}
