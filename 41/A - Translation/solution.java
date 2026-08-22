e0bfcee4723907fedd7429f2a4419ed280c5d534
HERAMB KRISHNA ARORA <heramb.24bce10154@vitbhopal.ac.in>
2026-08-22 18:30:11 +0530

    Add Translation [A] from Codeforces

---
+++ 41/A - Translation/solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        String s = sc.next();
+        String t = sc.next();
+ 
+        String reverse = new StringBuilder(s).reverse().toString();
+ 
+        if (reverse.equals(t)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
